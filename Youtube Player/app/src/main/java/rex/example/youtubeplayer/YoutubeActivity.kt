94df929f18729471abc344c3b92de3b9bf9a7c37
Hoaliti <rex_cyf_chen@users.noreply.github.com>
2019-10-25 17:32:45 -0400

    build youtube activity

---
+++ Youtube Player/app/src/main/java/rex/example/youtubeplayer/YoutubeActivity.kt
@@ -0,0 +1,55 @@
+package rex.example.youtubeplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+
+const val YOUTUBE_VIDEO_ID = "HPmD9I2b7L8"
+const val YOUTUBE_PLAYLIST = "PLzUDjjLEA9WTlVGgd0pDqvhkDaA2L9SeE"
+
+class YoutubeActivity : AppCompatActivity(), YouTubePlayer.OnInitializedListener{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val layout = layoutInflater.inflate(R.layout.activity_youtube,null) as ConstraintLayout
+        setContentView(layout)
+
+        val button1 = Button(this)
+        button1.layoutParams = ConstraintLayout.LayoutParams(600,180)
+        button1.text = "Button added"
+        layout.addView(button1)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(playerView)
+        playerView.initialize(getString(R.string.GOOGLE_API_KEY),this)
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        
+    }
+
+    override fun onInitializationFailure(
+        provider: YouTubePlayer.Provider?,
+        youTubeInitializationResult: YouTubeInitializationResult?
+    ) {
+        val REQUEST_CODE =0
+        if(youTubeInitializationResult?.isUserRecoverableError == true){
+            youTubeInitializationResult.getErrorDialog(this,REQUEST_CODE).show()
+        }else{
+            val errorMessage = "There was an error initializing the YoutubePlayer ($youTubeInitializationResult)"
+            Toast.makeText(this,errorMessage,Toast.LENGTH_LONG).show()
+        }
+    }
+}
