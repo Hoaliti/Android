f3e298d474aa959bebce5fee5b6898c01b6a9bf5
Hoaliti <rex_cyf_chen@users.noreply.github.com>
2019-11-05 14:25:32 -0500

    request app permissions
    WRITE EXTERNAL STORAGE

--- IMchat/app/src/main/java/rex/example/imchat/LoginActivity.kt
+++ IMchat/app/src/main/java/rex/example/imchat/LoginActivity.kt
@@ -1,13 +1,16 @@
 package rex.example.imchat
 
+import android.content.pm.PackageManager
 import android.view.KeyEvent
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 import rex.example.imchat.contract.LoginContract
 import rex.example.imchat.presenter.LoginPresenter
+import java.util.jar.Manifest
 
 class LoginActivity : BaseActivity(),LoginContract.View{
 
@@ -25,11 +28,40 @@ class LoginActivity : BaseActivity(),LoginContract.View{
     }
     fun login(){
         hideSoftKeyboard()
-        val userNameString = userName.text.trim().toString()
-        val passwordString = password.text.trim().toString()
+        if(hasWriteExternalStoragePermission()){
+            val userNameString = userName.text.trim().toString()
+            val passwordString = password.text.trim().toString()
 
-        presenter.login(userNameString,passwordString)
+            presenter.login(userNameString,passwordString)
+        }else{
+            applyWriteExternalStoragePermission()
+        }
 
+
+    }
+    //检查是否有写入磁盘的判断
+    private fun hasWriteExternalStoragePermission():Boolean{
+        val result = ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun applyWriteExternalStoragePermission(){
+        val permissions = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(this,permissions,0)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            //用户同意权限
+            login()
+        }else{
+            toast(R.string.permission_denied)
+        }
     }
     override fun onUserNameError() {
         userName.error = getString(R.string.user_name_error)
