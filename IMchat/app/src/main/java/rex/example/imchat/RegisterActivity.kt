30fbf2b0c392c9d312a6b76911c9cf70c6f9bfe2
Hoaliti <rex_cyf_chen@users.noreply.github.com>
2019-11-05 19:30:28 -0500

    Register View pattern

--- IMchat/app/src/main/java/rex/example/imchat/RegisterActivity.kt
+++ IMchat/app/src/main/java/rex/example/imchat/RegisterActivity.kt
@@ -1,8 +1,40 @@
 package rex.example.imchat
 
-class RegisterActivity : BaseActivity(){
+
+
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
+import rex.example.imchat.contract.RegisterContract
+
+class RegisterActivity : BaseActivity(),RegisterContract.View{
     override fun getLayoutResId(): Int {
         return R.layout.activity_register
+    }
+
+    override fun onUserNameError() {
+        userName.error = getString(R.string.user_name_error)
+    }
+
+    override fun onPasswordError() {
+        password.error = getString(R.string.password_error)
+    }
+
+    override fun onConfirmPasswordError() {
+        confirmPassword.error = getString(R.string.confirm_password_error)
+    }
+
+    override fun onStartRegister() {
+        showProgress(getString(R.string.registering))
+    }
+
+    override fun onRegisterSuccess() {
+        dismissProgress()
+        finish()
+    }
+
+    override fun onRegisterFailed() {
+        dismissProgress()
+        toast(R.string.register_failed)
     }
 
 }
