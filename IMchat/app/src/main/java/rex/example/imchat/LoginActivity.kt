7180c83f551ce24cc5b25952dc08509b8b1b3ae2
Hoaliti <rex_cyf_chen@users.noreply.github.com>
2019-11-05 19:19:53 -0500

    RegisterActivity

--- IMchat/app/src/main/java/rex/example/imchat/LoginActivity.kt
+++ IMchat/app/src/main/java/rex/example/imchat/LoginActivity.kt
@@ -18,6 +18,9 @@ class LoginActivity : BaseActivity(),LoginContract.View{
 
     override fun init() {
         super.init()
+        newUser.setOnClickListener{
+            startActivity<RegisterActivity>()
+        }
         login.setOnClickListener{
             login()
         }
---
+++ IMchat/app/src/main/java/rex/example/imchat/RegisterActivity.kt
@@ -0,0 +1,8 @@
+package rex.example.imchat
+
+class RegisterActivity : BaseActivity(){
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_register
+    }
+
+}
