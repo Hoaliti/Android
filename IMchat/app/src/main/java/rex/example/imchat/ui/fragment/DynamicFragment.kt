e2f657986f6c6c19e449510351eb5fa3e1bc1a4c
Hoaliti <rex_cyf_chen@users.noreply.github.com>
2019-11-06 11:51:13 -0500

    update dynamicFragment
    can logout from Firebase

--- IMchat/app/src/main/java/rex/example/imchat/ui/fragment/DynamicFragment.kt
+++ IMchat/app/src/main/java/rex/example/imchat/ui/fragment/DynamicFragment.kt
@@ -4,7 +4,12 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import kotlinx.android.synthetic.main.fragment_dynamic.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.support.v4.progressDialog
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 import rex.example.imchat.R
+import rex.example.imchat.ui.activity.LoginActivity
 
 class DynamicFragment :BaseFragment(){
     override fun getLayoutResId(): Int {
@@ -17,5 +22,20 @@ class DynamicFragment :BaseFragment(){
 
         val logoutString = String.format(getString(R.string.logout), FirebaseAuth.getInstance().currentUser!!.email)
         logout.text = logoutString
+
+        logout.setOnClickListener {
+            logout()
+        }
+    }
+
+    private fun logout(){
+        FirebaseAuth.getInstance().signOut()
+        context!!.runOnUiThread {
+            toast(R.string.logout_success)
+            this.startActivity<LoginActivity>()
+            activity!!.finish()
+        }
+
+
     }
 }
