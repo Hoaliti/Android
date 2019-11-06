ceb116b482382467873982abe89b1a19a7393a0c
Hoaliti <rex_cyf_chen@users.noreply.github.com>
2019-11-06 11:44:52 -0500

    dynamic layout

--- IMchat/app/src/main/java/rex/example/imchat/ui/fragment/DynamicFragment.kt
+++ IMchat/app/src/main/java/rex/example/imchat/ui/fragment/DynamicFragment.kt
@@ -1,9 +1,21 @@
 package rex.example.imchat.ui.fragment
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import kotlinx.android.synthetic.main.fragment_dynamic.*
+import kotlinx.android.synthetic.main.header.*
 import rex.example.imchat.R
 
 class DynamicFragment :BaseFragment(){
     override fun getLayoutResId(): Int {
         return R.layout.fragment_dynamic
+    }
+
+    override fun init() {
+        super.init()
+        headerTitle.text = getString(R.string.dynamic)
+
+        val logoutString = String.format(getString(R.string.logout), FirebaseAuth.getInstance().currentUser!!.email)
+        logout.text = logoutString
     }
 }
