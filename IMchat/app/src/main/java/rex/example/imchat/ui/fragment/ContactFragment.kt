f30c95e05fae0521c921ec1e456010d0f9b1aa9a
Hoaliti <rex_cyf_chen@users.noreply.github.com>
2019-11-06 11:55:21 -0500

    Update ContactFragment
    adding swipeRefresh function
    set Title text

--- IMchat/app/src/main/java/rex/example/imchat/ui/fragment/ContactFragment.kt
+++ IMchat/app/src/main/java/rex/example/imchat/ui/fragment/ContactFragment.kt
@@ -1,9 +1,24 @@
 package rex.example.imchat.ui.fragment
 
+import android.graphics.Color
+import android.view.View
+import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.header.*
 import rex.example.imchat.R
 
 class ContactFragment : BaseFragment(){
     override fun getLayoutResId(): Int {
         return R.layout.fragment_contacts
+    }
+
+    override fun init() {
+        super.init()
+        headerTitle.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+        swipeRefreshLayout.apply {
+            swipeRefreshLayout.setColorSchemeColors(Color.BLUE)
+            isRefreshing = true
+        }
+
     }
 }
