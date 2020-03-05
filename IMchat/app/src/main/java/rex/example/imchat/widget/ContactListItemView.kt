9d91c291cf81579ae1f94734fefea81855fe1d68
Hoaliti <rex_cyf_chen@users.noreply.github.com>
2020-03-04 22:53:50 -0500

    commit

---
+++ IMchat/app/src/main/java/rex/example/imchat/widget/ContactListItemView.kt
@@ -0,0 +1,10 @@
+package rex.example.imchat.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class ContactListItemView(context: Context?,attrs:AttributeSet?=null)
+    :RelativeLayout(context,attrs){
+
+}
